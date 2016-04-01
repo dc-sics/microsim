bfa776ea728509123b4cd6d26dde2aa52d330b7f
Christian Mesch <christianmesch@gmail.com>
2016-04-01 11:54:39 +0200

    Small fixes and indentations

--- SimulationWorker/src/main/java/christianmesch/simulationworker/misc/Pair.java
+++ SimulationWorker/src/main/java/christianmesch/simulationworker/misc/Pair.java
@@ -7,40 +7,46 @@ package christianmesch.simulationworker.misc;
 
 import java.util.Objects;
 
+/**
+ *
+ * @author Christian Mesch
+ * @param <A>
+ * @param <B>
+ */
 public class Pair<A, B> {
-    
-    public final A a;
-    public final B b;
-    
-    public Pair(A a, B b) {
-	this.a = a;
-	this.b = b;
-    }
-    
-    @Override
+
+	public final A a;
+	public final B b;
+
+	public Pair(A a, B b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
 	public int hashCode() {
-	return Objects.hash(a,b);
-    }
-    
-    @Override
+		return Objects.hash(a, b);
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-	if (this == obj) {
-	    return true;
-	}
-	if (obj == null) {
-	    return false;
-	}
-	if (obj instanceof Pair) {
-	    final Pair<?, ?> other = (Pair<?, ?>) obj;
-	    if (!Objects.equals(this.a, other.a)) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (obj instanceof Pair) {
+			final Pair<?, ?> other = (Pair<?, ?>) obj;
+			if (!Objects.equals(this.a, other.a)) {
+				return false;
+			}
+			if (!Objects.equals(this.b, other.b)) {
+				return false;
+			}
+			return true;
+		}
 		return false;
-	    }
-	    if (!Objects.equals(this.b, other.b)) {
-		return false;
-	    }
-	    return true;
-	} 
-	return false;
-    }
-    
+	}
+
 }
--- SimulationWorker/src/main/java/christianmesch/simulationworker/misc/Triple.java
+++ SimulationWorker/src/main/java/christianmesch/simulationworker/misc/Triple.java
@@ -12,44 +12,44 @@ import java.util.Objects;
  * @author Mark Clements
  */
 public class Triple<A, B, C> {
-    
-    public final A a;
-    public final B b;
-    public final C c;
-    
-    public Triple(A a, B b, C c) {
-	this.a = a;
-	this.b = b;
-	this.c = c;
-    }
-    
-    @Override
+
+	public final A a;
+	public final B b;
+	public final C c;
+
+	public Triple(A a, B b, C c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	@Override
 	public int hashCode() {
-	return Objects.hash(a, b, c);
-    }
-    
-    @Override
+		return Objects.hash(a, b, c);
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-	if (this == obj) {
-	    return true;
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (obj instanceof Triple) {
+			final Triple<?, ?, ?> other = (Triple<?, ?, ?>) obj;
+			if (!Objects.equals(this.a, other.a)) {
+				return false;
+			}
+			if (!Objects.equals(this.b, other.b)) {
+				return false;
+			}
+			if (!Objects.equals(this.c, other.c)) {
+				return false;
+			}
+			return true;
+		}
+		return false;
 	}
-	if (obj == null) {
-	    return false;
-	}
-	if (obj instanceof Triple) {
-	    final Triple<?, ?, ?> other = (Triple<?, ?, ?>) obj;
-	    if (!Objects.equals(this.a, other.a)) {
-		return false;
-	    }
-	    if (!Objects.equals(this.b, other.b)) {
-		return false;
-	    }
-	    if (!Objects.equals(this.c, other.c)) {
-		return false;
-	    }
-	    return true;
-	} 
-	return false;
-    }
-    
+
 }
